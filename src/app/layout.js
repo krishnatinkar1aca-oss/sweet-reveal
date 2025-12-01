ea09c90e5e47c187019a77792ea1a1f0aac6c1f1
krishnatinkar1aca-oss <krishnatinkar1aca@gmail.com>
2025-12-01 23:40:10 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,9 @@
-import { Kalam } from "next/font/google";
-import "./globals.css";
-
-const kalam = Kalam({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-});
-
-export const metadata = {
-  title: "Something Special!",
-  description: "A romantic message just for you",
-};
+import React from 'react';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${kalam.className} antialiased select-none`}
-      >
+      <body>
         {children}
       </body>
     </html>
